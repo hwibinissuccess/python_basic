5bbab34f8774bc987f24a184c5ef14d83cbe2107
Choi Hwi bin <chlgnlqls3@nate.com>
2024-07-26 17:48:58 +0900

    python 기초 study

---
+++ MyPython/sec07/b_op.py
@@ -0,0 +1,16 @@
+#### 연습 : 자료형을 확장해보자, UI 확장, 속성 정의 확장
+
+class MyList(list):
+    # 기능추가
+    def sum(self):
+        return sum(self) #합을 구하는 내장 함수를 호출
+
+    def average(self):
+        return sum(self) / len(self)
+
+if __name__ == '__main__':
+    mlist = MyList([1,2,3,4,5])
+    print(dir(mlist))
+    print(mlist, type(mlist))
+    print(f'sum :  {mlist.sum()}')
+    print(f'average : {mlist.average()}')
---
+++ MyPython/sec07/d_abstract.py
@@ -0,0 +1,40 @@
+# from _typeshed import Self
+from abc import abstractmethod, ABCMeta
+
+'''
+ ***추상메소드를 가진 클래스는 추상클래스가 되고 객체 생성 불가능 *** 
+ (Can't instantiate abstract class AA without an implementation for abstract method 'prn')
+  1)선조가 가진 추상메소드를 후손이 재정의하지 않으면 객체를 생성할 수 없다
+  2)재정의하지 않는 후손은 추상클래스를 된다.  
+
+  Tip: 추상클래스란? 다형성과 동적 바인딩(다양한 형태의 성질(기능)을 가진 후손 클래스들을 추상 메소드로 통일 하는 구조 )
+       - 추상클래스 객체 생성 불가, 후손 추상메소드 반드시 재정의, 재정의하지 않는 후손은 추상클래가 된다.   
+       
+    My(metaclass = ABCMeta):
+    @abstractmethod
+    prn() : 기능없는 메소드로 추상화
+
+'''
+
+class My(metaclass=ABCMeta): # 선언부부터 나는 추상클래스야, 객체 생성 안하고 후손 클래스에게 추상메소드를 강제로 재정의하겠다.
+    @abstractmethod
+    def prn(self): #추상 메소드 / 후손 강제 재정의하겠다.
+        pass
+
+    def test(self):
+        print("중요한 메소드")
+
+class Test(My):
+    def m(self):
+        print("Test'm method")
+
+    def prn(self):
+        print("재정의 메소드")
+
+if __name__ == '__main__':
+    #m = My()
+    t = Test()
+    t.m()
+    t.test()
+    t.prn()
+
